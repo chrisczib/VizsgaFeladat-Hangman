704f16cc38de1657dd39ec76caa3bcd5d833b287
chrisczib <czibere.christopher@gmail.com>
2022-03-29 21:27:28 +0200

    final before moving it to netbeans

--- PHP/hangman.php
+++ PHP/hangman.php
@@ -1,14 +1,20 @@
 <!-- PHP RETRIEVES DATA THAT AJAX WILL USE IN THE JS FILE-->
 
-<?php
-// connect to database and retrieve the words 
-$sql = "SELECT id, word, category FROM hangman";
-            $result = $connect ->query($sql);
-            if ($result->num_rows > 0){
-                    while ($row = $result -> fetch_assoc($result)){
-                        echo "<p>";
-                        echo $row['word'];
-                        echo "</p>";
-                }
-            }
+<?php   
+    $mysqli = new mysqli("localhost", "root", "", "akasztofa");
+
+    if($mysqli->connect_errno){
+        echo 'Hiba a csatlakozáskor!';
+        exit();
+    }
+    $mysqli->set_charset("utf8");
+    $sql = "SELECT szo FROM vizsga";
+    $result = $mysqli ->query($sql);
+    if ($result->num_rows > 0){
+            while ($row = $result -> fetch_assoc()){
+                echo "<p>";
+                echo $row['szo'];
+                echo "</p>";
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php   
-    $mysqli = new mysqli("localhost", "root", "", "akasztofa");
-
-    if($mysqli->connect_errno){
-        echo 'Hiba a csatlakozáskor!';
-        exit();
-    }
-    $mysqli->set_charset("utf8");
-    $sql = "SELECT szo FROM vizsga";
-    $result = $mysqli ->query($sql);
-    if ($result->num_rows > 0){
-            while ($row = $result -> fetch_assoc()){
-                echo "<p>";
-                echo $row['szo'];
-                echo "</p>";
-        }
-    }
+   require 'hangman.php';
 ?>
 
 <!DOCTYPE html>
